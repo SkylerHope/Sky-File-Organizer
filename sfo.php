20b7c6c6a5206119e017cb90d968f23890fd738a
SkylerHope <142663439+SkylerHope@users.noreply.github.com>
2023-12-01 18:53:52 +0200

    Add files via upload
    Added an option for users to organize all files in the specified directory

--- sfo.php
+++ sfo.php
@@ -9,6 +9,12 @@ function organizeFiles($directory, $fileTypes) {
         return;
     }
 
+    // Check if the user wants to organize all files [NEW]
+    $organizeAllFiles = false;
+    if(count($fileTypes) == 1 && trim($fileTypes[0]) === 'A') {
+        $organizeAllFiles = true;
+    }
+
     // Check if valid file types are provided
     if ($fileTypes == ['']) {
         echo "Error: Provide valid file types!\n\n";
@@ -18,11 +24,15 @@ function organizeFiles($directory, $fileTypes) {
     // Get the list of files in the directory
     $files = scandir($directory);
 
-    // Filter files based on specified file types
-    $filesToOrganize = array_filter($files, function ($file) use ($fileTypes) {
-        $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
-        return in_array($fileExtension, $fileTypes);
-    });
+    // Filter files based on specified file types or organize all files [MODIFIED]
+    $filesToOrganize = ($organizeAllFiles)
+        ? array_filter($files, function ($file) {
+            return $file !== '.' && $file !== '..';
+        })
+        : array_filter($files, function ($file) use ($fileTypes) {
+            $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
+            return in_array($fileExtension, $fileTypes);
+        });
 
     // Check if no files with specified types are found in the directory
     if (empty($filesToOrganize)) {
@@ -77,6 +87,7 @@ echo "\n  .-')
 // Main loop to continuously get user input for directory and file types
 while (true) {
     $directory = readline("Directory Path (CTRL+C to exit): ");
+    echo "\nType [A] to organize all files\n"; // [NEW]
     $fileTypes = explode(',', readline("File Types (comma-separated without spaces): "));
 
     // Call the organizeFiles function with the provided inputs
